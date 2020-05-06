1cfcfd8c099c2676e5fcbc224ba14b2cfd024b5e
Daniela Aliaga <raraliaga@gmail.com>
2020-05-05 21:40:15 -0400

    Integración evento smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 $(function () {
+  
+  $("a").click(function(event){
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hashtag = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(hashtag).offset().top
+      }, 800, function(){
+        window.location.hash = hashtag;
+      });
+    }
+  });
+
     $('[data-toggle="tooltip"]').tooltip()
   })
